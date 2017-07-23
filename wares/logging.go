b111576dac0e8f9f14d6d40a75c56dd3672b04e4
riley rook <rileyrook@gmail.com>
2017-07-23 07:58:49 -0500

    Add more stuff to logs

--- wares/logging.go
+++ wares/logging.go
@@ -3,16 +3,32 @@ package wares
 import (
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/felixge/httpsnoop"
 	"github.com/julienschmidt/httprouter"
 )
 
 func Logging(fn httprouter.Handle) httprouter.Handle {
 	return func(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		reqID, _ := r.Context().Value(RequestIDContextKey).(string)
-		method := r.Method
-		log.Printf("START %s - %s\n", method, reqID)
-		fn(w, r, p)
-		log.Printf("END %s - %s\n", method, reqID)
+		var code int
+
+		log.Printf("START id=%s method=%s url=%s\n", reqID, r.Method, r.URL)
+		defer func(ts time.Time) {
+			duration := time.Since(ts).String()
+			if code == 0 {
+				code = 500
+			}
+
+			log.Printf("END id=%s method=%s code=%d duration=%s\n", reqID, r.Method, code, duration)
+		}(time.Now())
+
+		wrapper := func(w2 http.ResponseWriter, r2 *http.Request) {
+			fn(w2, r2, p)
+		}
+
+		m := httpsnoop.CaptureMetrics(http.HandlerFunc(wrapper), w, r)
+		code = m.Code
 	}
 }
